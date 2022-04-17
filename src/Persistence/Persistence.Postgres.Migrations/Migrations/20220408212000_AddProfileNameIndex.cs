d704905f3b9adad2481a17df2721ab73dc83adcc
KungurtsevNI <kungurtsevni@gmail.com>
2022-04-18 00:07:45 +0400

    index in migrations

---
+++ src/Persistence/Persistence.Postgres.Migrations/Migrations/20220408212000_AddProfileNameIndex.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Persistence.Postgres.Migrations.Migrations;
+
+[Migration(20220418212000)]
+public sealed class AddProfileNameIndex : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql(@"
+           CREATE INDEX lower_last_and_first_name_inx ON profiles (lower(last_name) text_pattern_ops, lower(first_name) text_pattern_ops);
+        ");
+    }
+
+    public override void Down()
+    {
+        Execute.Sql(@"
+           DROP INDEX lower_last_and_first_name_inx;
+        ");
+    }
+}
